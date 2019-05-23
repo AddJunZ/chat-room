a72faed1f04b1282f591bf978ef31098000ac065
AddJun <1574118221@qq.com>
2019-05-23 20:13:07 +0800

    post可以拿到登录者了

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const Koa = require('koa');
 const server = new Koa();
 const Router = require('koa-router');
 const router = new Router();
-const IO = require('koa-socket');
+const IO = require('koa-socket');//实现对websocket的对接的
 const io = new IO();
+const render = require('koa-ejs');//模板
+const path = require('path');//为了写模板的路径引入的
+const body = require('koa-better-body');
+
+server.use(body({
+    uploadDir:'./static/upload'
+}))
+
+//这样一挂后，路由里都能使用了？
+render(server,{
+    root:path.join(__dirname,'views'),
+    layout:false,
+    viewExt:'html',
+    cache:false,
+    debug:false
+})
+
+
 
 router.use('', require('./routes/index.js'))
 server.use(router.routes())
@@ -31,11 +49,12 @@ io.use(async (ctx, next) => {
 
 //在这里监听html触发的事件
 //登录后就跳到聊天界面
-// server.io.on('login',ctx=>{
+server.io.on('login',ctx=>{
     // console.log('login');
-    //路由的ctx才有跳转
+    // 路由的ctx才有跳转
     // ctx.redirect('/chat');
-// })
+    // io.b
+})
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,21 @@ const fs = require('fs')
 // router.prefix('/login');
 
 router.get('/',async ctx=>{
-    ctx.type = 'html',
-    ctx.body = fs.createReadStream('views/login.html')
+    // ctx.type = 'html',
+    // ctx.body = fs.createReadStream('views/login.html')
+    await ctx.render('login');//使用模板后
 })
-
 
 
 //get 和 post都可能用all，但是单独一个就要和表单的请求方式一致
 router.post('/choose',async ctx=>{
+    console.log('按道理请求的数据会在这里？',ctx.request.fields);//undefined???
+
     ctx.type = 'html',
     ctx.body = fs.createReadStream('views/choose.html')
 })
+
+
 
 // router.all('/chat',async ctx=>{
 //     ctx.type = 'html',
